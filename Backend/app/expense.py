273653dc3c87cadc866d6745cdbed59e2479bd3a
Rqqureshi <rafayahmedqureshi60@gmai.com>
2026-08-05 18:58:33 +0300

    Create backend architecture

--- Backend/app/expense.py
+++
@@ -1,9 +0,0 @@
-class Expense:
-    def __init__(self, name, category, amount) -> None: 
-        self.name = name
-        self.category = category
-        self.amount = amount
-
-
-    def __repr__(self) -> str:
-        return f"Expense(name= {self.name}, category= {self.category}, amount= {self.amount})"
